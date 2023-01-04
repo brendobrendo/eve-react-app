0508c47340ebaad0a33520f7834325dc2d7a95e9
brendobrendo <brendan.smith903@gmail.com>
2023-01-04 16:54:09 -0500

    start script echo adds

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
+echo about to start the start
 npm start
+echo about to start pm2
 pm2 start npm --name "eve-react-app" -- start
+echo about to startup
 pm2 startup
+echo about to save
 pm2 save
+echo about to restart all
 pm2 restart all
