2efdf532939d5739a42906e72c616929d8cb3a63
brendobrendo <brendan.smith903@gmail.com>
2023-01-17 07:19:03 -0800

    changes to app_start

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
-echo lsof -i tcp:3000
-echo About to start npm
 npm start
-echo finished npm start
 pm2 start npm --name "eve-react-app" -- start
 pm2 startup
 pm2 save
